42ca3184e7f5a9e05ce496cc4f8e3166db2afcce
caymiferreira <caymiferreira@gmail.com>
2022-12-06 16:01:07 -0300

    primeiro commit

---
+++ bluebird.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+largura_tela = 640
+altura_tela = 480
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption('BLUE BIRD')
+
+fundo = pygame.image.load('')
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit
+            exit()
+    pygame.display.update()
