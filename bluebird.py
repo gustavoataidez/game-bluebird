0bddc6966b575f4616b162174deaabcf99cd7f63
caymiferreira <caymiferreira@gmail.com>
2022-12-06 17:02:41 -0300

    imagem de fundo

--- bluebird.py
+++ bluebird.py
@@ -9,7 +9,8 @@ altura_tela = 480
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption('Blue Bird')
 
-fundo = pygame.image.load('')
+fundo = pygame.image.load('back2 (1).png')
+fundo = pygame.transform.scale(fundo, (largura_tela, altura_tela))
 
 while True:
 
@@ -18,3 +19,5 @@ while True:
             pygame.quit
             exit()
     pygame.display.update()
+
+    tela.blit(fundo, (0, 0))
