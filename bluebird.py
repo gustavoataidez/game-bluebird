d14fead114073c4513d43bf476c2e29cf51708ad
caymiferreira <caymiferreira@gmail.com>
2022-12-07 15:55:18 -0300

    passaro, velocidade e gravidade

--- bluebird.py
+++ bluebird.py
@@ -6,18 +6,63 @@ pygame.init()
 
 largura_tela = 640
 altura_tela = 480
+velocidade = 10
+gravidade = 1
+
+class Passaro(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.images = [pygame.image.load('testeupflap.png').convert_alpha(),
+                       pygame.image.load('testemidflap.png').convert_alpha(),
+                       pygame.image.load('testedownflap.png').convert_alpha()]
+
+        self.velocidade = velocidade
+
+        self.current_image = 0
+
+        self.image = pygame.image.load('testeupflap.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect[0] = largura_tela / 2
+       
+        self.rect[1] = altura_tela / 2
+    def update(self):
+        self.current_image = (self.current_image + 1) % 3
+        self.image = self.images [self.current_image]
+
+        self.velocidade += gravidade
+
+        #update altura
+        self.rect[1] += self.velocidade
+
+    def pulo(self):
+        self.velocidade = -velocidade
+
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption('Blue Bird')
 
-fundo = pygame.image.load('imagemfundo.xcf')
+fundo = pygame.image.load('imagem de fundo.png')
 fundo = pygame.transform.scale(fundo, (largura_tela, altura_tela))
 
-while True:
+grupopassaro = pygame.sprite.Group()
+passaro = Passaro()
+grupopassaro.add(passaro)
 
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit
             exit()
-    pygame.display.update()
-
+        if event.type == KEYDOWN:
+              if event.key == K_SPACE:
+                passaro.pulo()  
     tela.blit(fundo, (0, 0))
+    
+    grupopassaro.update()
+    grupopassaro.draw(tela)
+
+    pygame.display.update()
